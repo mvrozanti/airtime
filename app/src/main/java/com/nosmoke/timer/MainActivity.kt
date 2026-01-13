e17e5658a43d5296dff8d84be79ce7cd47a4b8a0
Marcelo <mvrozanti@hotmail.com>
2026-01-13 00:20:06 -0300

    Add Log import to MainActivity

--- app/src/main/java/com/nosmoke/timer/MainActivity.kt
+++ app/src/main/java/com/nosmoke/timer/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
